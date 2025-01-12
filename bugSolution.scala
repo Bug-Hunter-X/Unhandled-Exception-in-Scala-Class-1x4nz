1fea28087447b72b02c9ebd93cef21de2784d42b
BugHunter-X <askzenai@gmail.com>
2025-01-12 10:06:07 +0100

    Add bugSolution.scala file

---
+++ bugSolution.scala
@@ -0,0 +1,27 @@
+```scala
+class MyClass(val name: String) {
+  private var _age: Int = 0
+
+  def age: Int = _age
+  def age_=(newAge: Int): Unit = {
+    if (newAge > 0) {
+      _age = newAge
+    } else {
+      throw new IllegalArgumentException("Age must be positive")
+    }
+  }
+}
+
+object Main extends App {
+  val myObj = new MyClass("John")
+  println(myObj.age) // Output: 0
+  myObj.age = 30
+  println(myObj.age) // Output: 30
+  try {
+    myObj.age = -10 // This will throw an exception
+    println(myObj.age)
+  } catch {
+    case e: IllegalArgumentException => println(s"Error: "+e.getMessage)
+  }
+}
+```
